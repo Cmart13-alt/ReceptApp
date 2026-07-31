7c72c39b3fc639c5798ff17517e375c740108a15
Mårten Sundén <ivossn@gmail.com>
2026-07-31 14:17:49 +0200

    Publish 1785500269647

--- Reader/service-worker.js
+++ Reader/service-worker.js
@@ -1,16 +1,17 @@
 
-const CACHE_NAME = "recipe-reader-1785500136709";
+const CACHE_NAME = "recipe-reader-1785500269647";
 
 const FILES = [
-    "/css/style.css",
-    "/data/recipes.json",
-    "/data/version.json",
-    "/icons/icon-192.png",
-    "/icons/icon-512.png",
-    "/icons/maskable-icon-512.png",
-    "/index.html",
-    "/js/app.js",
-    "/manifest.json"
+    "./css/style.css",
+    "./data/recipes.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png",
+    "./icons/maskable-icon-512.png",
+    "./index.html",
+    "./js/app.js",
+    "./manifest.json",
+    "./service-worker.js",
+    "./version.json"
 ];
 
 self.addEventListener("install", event => {
@@ -55,6 +56,27 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
 
+    const url = new URL(event.request.url);
+
+    //
+    // Hämta alltid senaste versionsfilen
+    //
+
+    if (
+        url.pathname.endsWith("/version.json") ||
+        url.pathname.endsWith("/data/version.json")
+    ) {
+
+        event.respondWith(fetch(event.request));
+
+        return;
+
+    }
+
+    //
+    // Övriga filer hämtas från cachen
+    //
+
     event.respondWith(
 
         caches.match(event.request)
@@ -63,3 +85,4 @@ self.addEventListener("fetch", event => {
     );
 
 });
+
